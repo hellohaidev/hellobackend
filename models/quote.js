5eac1a4280083d3b84f13b06713f9de3ed82f8b2
adirahman <kodingku@github.com>
2018-11-29 10:57:05 +0700

    update model quote

--- models/quote.js
+++ models/quote.js
@@ -1,7 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const quote = sequelize.define('quote', {
-    title: DataTypes.STRING,
     description: DataTypes.TEXT
   }, {});
   quote.associate = function(models) {
