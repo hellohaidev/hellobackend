e56d1848e6ac8754a1b0bc4faf07a942ca84e7cc
adirahman <kodingku@github.com>
2018-11-30 18:29:08 +0700

    modificate app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const attitudeRouter = require('./routes/attitude')
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', indexRouter);
-app.use('/users', usersRouter);
+app.use('/attitude', attitudeRouter);
 
 
 app.listen(process.env.PORT ||  5000, function () {
