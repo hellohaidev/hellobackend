2a8d78162e3b86a1b4150a686478ececa640278d
adirahman <kodingku@github.com>
2018-12-28 08:25:23 +0700

    upload migration table map

---
+++ migrations/20181227064335-create-tblmap.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('tblmaps', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      latitude: {
+        type: Sequelize.FLOAT
+      },
+      longtitude: {
+        type: Sequelize.FLOAT
+      },
+      kode: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('tblmaps');
+  }
+};
