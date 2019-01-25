061e44ef8717b647fd21c8d933b1c91f6bccb464
adirahman <kodingku@github.com>
2019-01-25 15:22:11 +0700

    upload maps

--- routes/maps.js
+++ routes/maps.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router();
+const request = require('request')
+const axios = require('axios')
 const {tblmap} = require('../models');
 
 
 
+/**
+ * =================================================
+ * @author Adi Rahman                               
+ * @description List All service API google maps
+ * =================================================
+ */
+
+/**
+ * @description Service Google Maps API place marker  
+ */
 
 router.get('/',async(req,res)=>{
     try {
@@ -71,6 +83,26 @@ router.delete('/:id',async(req,res)=>{
         res.json('sukses hapus data ' + data)   
     } catch (error) {
         console.log(error)
+    }
+})
+
+/**
+ * @description Service distance google maps Api
+ */
+
+router.get('/route',async(req,res)=>{
+
+    try {
+
+        let idorigin = req.query.origin
+        let iddestination = req.query.destination
+        let url = `https://maps.googleapis.com/maps/api/directions/json?origin=${idorigin}&destination=${iddestination}&key=AIzaSyDI6V_nRIn8zAzfJuBNpzdQbV33jWWb4Ps`
+        const data = await axios.get(url)
+        let hasildata = data.data
+        res.json(hasildata)
+
+    } catch (error) {
+        res.json(error)
     }
 })
 
